b508ce11b3de4da60e2d5a854f810d73c135fc5c
Alexey Sulatskov <sulatskovalex@gmail.com>
2021-10-13 13:27:47 +0300

    Add error

--- errs/errors.go
+++ errs/errors.go
@@ -3,9 +3,11 @@ package errs
 import "github.com/twitchtv/twirp"
 
 const (
-	NotFoundMsg = "not found"
+	NotFoundMsg           = "not found"
+	UnknownContentTypeMsg = "unknown content type"
 )
 
 var (
-	NotFoundErr = twirp.NewError(twirp.NotFound, NotFoundMsg)
+	NotFoundErr           = twirp.NewError(twirp.NotFound, NotFoundMsg)
+	UnknownContentTypeErr = twirp.NewError(twirp.InvalidArgument, UnknownContentTypeMsg)
 )
